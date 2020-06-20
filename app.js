4f082cf5fa5ae371cdf6e1f084e7c12ad18b9c0d
Marie Morin <mariemorin19@gmail.com>
2020-06-20 10:28:23 +0200

    Projets 7 to 11

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ displayDate();
 mesureArea(5, 6, 7);
 animateString("target");
 isLeapYear();
+findJanuaryFirst();
+findRandomNumber();
+howLongBeforeChristmas()
+multiplyOrDivide();
+fahrenheitOrCelsius(document.querySelectorAll(".celsiusAndFahrenheit input"));
 
 function displayDateAndHour() {
     const dateElement = document.querySelector(".date-hour");
@@ -92,5 +97,106 @@ function isLeapYear() {
         }
 
         answerPara.textContent += yearValue + " is " + answer;
+    })
+}
+
+function findJanuaryFirst() {
+    let januaryFirsts = [];
+    let year = 2014;
+
+    while (year < 2050) {
+        year++;
+        let date = new Date(year, 0, 1);
+        let day = date.getDay();
+        if (day === 0) {
+            januaryFirsts.push(year);
+        }
+    }
+    let answer = document.createElement("p");
+    let januaryYears = januaryFirsts.toString().split(",").join(", ");;
+    answer.textContent = `In ${januaryYears}, January 1st have been or will be a Sunday.`;
+    document.querySelector(".januaryFirst").appendChild(answer);
+}
+
+function findRandomNumber() {
+    let tries = 3;
+
+    document.querySelector(".findRandomNumber button").addEventListener("click", function () {
+        let randomNumber = Math.round(Math.random() * 10);
+        console.log(randomNumber);
+        while (tries > 0) {
+            let userGuess = prompt("Guess a number between 0 and 10 ?");
+
+            if (userGuess > randomNumber) {
+                alert('Your guess is too high.');
+            } else if (userGuess < randomNumber) {
+                alert('Your guess is too low');
+            } else {
+                alert("Good work !");
+                break;
+            }
+            tries = tries - 1;
+        }
+        if (tries == 0) {
+            alert('You ran out of tries.  The number was ' + randomNumber + '.');
+        }
+    });
+}
+
+function howLongBeforeChristmas() {
+    const today = new Date();
+    const currentYear = today.getFullYear();
+    const christmas = new Date(currentYear, 11, 25);
+
+    if (today.getMonth == 11 && today.getDay > 25) {
+        christmas.setFullYear(christmas.getFullYear + 1);
+    }
+
+    const millisecondsInAHour = 86400000;
+    let daysLeft = christmas.getTime() - today.getTime();
+    daysLeft = Math.ceil(daysLeft / millisecondsInAHour);
+    const answerElt = document.querySelector(".christmasAnswer");
+    answerElt.textContent = `They are ${daysLeft} days left until next Christmas.`;
+
+
+
+}
+
+function multiplyOrDivide() {
+    let resultElt = document.getElementById("resultElt");
+    const buttons = document.querySelectorAll(".multiplyOrDivide button");
+    buttons.forEach(button => {
+
+        button.addEventListener("click", function () {
+            resultElt.textContent = "";
+            const firstNumber = document.getElementById("firstNumber").value;
+            const secondNumber = document.getElementById("secondNumber").value;
+            let answer = null;
+            if (button.dataset.operation == "/") {
+                answer = firstNumber / secondNumber;
+            } else {
+                answer = firstNumber * secondNumber;
+            }
+            resultElt.textContent += answer;
+        })
+    })
+}
+
+function fahrenheitOrCelsius(inputs) {
+    const celsius = document.querySelector('input[data-degre="c"]');
+    const fahrenheit = document.querySelector('input[data-degre="f"]');
+
+    inputs.forEach(input => {
+        input.addEventListener("input", function (e) {
+            let number = e.target.value;
+
+            if (input.dataset.degre == "f") {
+                let resultat = Math.round(((number - 32) / 9) * 5);
+                celsius.value = resultat;
+            } else {
+                let resultat = Math.round(((number / 5) * 9) + 32);
+                fahrenheit.value = resultat;
+            }
+        })
     })
 }
